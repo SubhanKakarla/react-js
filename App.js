72f34c766fe3b581e54fc5608cad71ac44626cc1
Subhan Kakarla <subhankakarla0912.com>
2025-01-06 05:06:52 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 // Nested elements
 
 const parent = React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
        // React.createElement("h1",{},"I'm a H1 Tag")
-      [ React.createElement("h1",{},"I'm a H1 Tag"),
-        React.createElement("h2",{},"I'm a H2 Tag") ]
+      [ React.createElement("h1",{},"I'm a h1 Tag"),
+        React.createElement("h2",{},"I'm a h2 Tag") ]
     )
 )
-
 
 console.log(parent) //Object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+
 root.render(parent);
