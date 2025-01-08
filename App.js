07aaa19d7ede8ddde095ef59e3c03d9242a0fb8b
Subhan Kakarla <subhankakarla0912.com>
2025-01-08 15:29:22 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// Nested elements
 
-const parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-       // React.createElement("h1",{},"I'm a H1 Tag")
-      [ React.createElement("h1",{},"I'm a h1 Tag"),
-        React.createElement("h2",{},"I'm a h2 Tag") ]
-    )
-)
+// JSX => HTML like syntax
 
-console.log(parent) //Object
+const elem = <span>React element</span>
+const Title = () => (
+    
+<h1 className="Head"  id="heading">{elem} Hello world using JSX !!!</h1>
+);
+
+
+//React Functional component
+
+const HeadingComponent1 = () => {
+    return <h1>React Functional Component</h1>
+};
+
+const HeadingComponent = () =>  (
+<div>
+    <Title/>
+    <Title></Title>
+    {Title()}
+        <h1>React Functional Component</h1>
+</div>
+ ) ;
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+// root.render(jsxHeading)
+root.render(< HeadingComponent />)
